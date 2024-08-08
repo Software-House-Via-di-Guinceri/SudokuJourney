9647e0c08e88786b950a1f2c0914384c87826f1e
nicobboi <ni.ragazzi@gmail.com>
2024-08-08 20:07:19 +0200

    first backend commit

---
+++ backend/src/main/java/com/shvg/sudoku_journey/SudokuJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.shvg.sudoku_journey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokuJourneyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shvg/sudoku_journey/SudokuJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shvg.sudoku_journey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
